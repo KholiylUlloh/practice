bf59559293c3aae60d87da929c2327950796eb37
AhmadjanovD <halilullahahmadjanov@gmail.com>
2022-07-20 02:25:59 +0900

    useReducer

--- src/root.jsx
+++ src/root.jsx
@@ -1,27 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux';
-import { increament, decreament } from './redux/createSlice';
-import { getPost } from './redux/extraReducer'
+import React, {useState, useReducer} from 'react'
+
 
 const Root = () => {
-    // const counter = useSelector((store) => store.counter);
-    const user = useSelector((store) => store.user);
-    const dispatch = useDispatch()
-
-    useEffect(()=>{
-         dispatch(getPost());
-    }, [])
+  const [amount, setAmount] = useState(localStorage.getItem('value')|| 2);
+  const reducer = (st, action)=>{
+    switch(action.type){
+      case 'plus':
+        localStorage.setItem('count', st + 1);
+        return st + 1;
+      case 'minus':
+        return st - 1
+      case 'byamount':
+        localStorage.setItem('count',st + +action.payload.amount)
+        return st + +action.payload.amount
+        default: return st;
+    }
+  }
+  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, +localStorage.getItem('count') || 0);
   return (
-    <div style={{ width:'100%',display:'grid', gridTemplateColumns:'auto auto auto auto'}}>
-       {
-        user.value.map((v)=>{
-            return(
-                <h1 key={v.id}>{v.title}</h1>
-            )
-        })
-       }
+    <div>
+       <h1>UseState:{count}</h1>
+       <button onClick={()=>setCount(count + 1)}>+</button>
+       <button onClick={()=>setCount(count - 1)}>-</button>
+       <h1>UseReducer:{localStorage.getItem('count')||state}</h1>
+       <button onClick={()=>dispatch({type: 'plus'})}>+</button>
+       <button onClick={()=>dispatch({type: 'minus'})}>-</button>
+       <select name="" id="" onChange={({target})=>setAmount(localStorage.setItem('value',target.value))}
+        defaultValue={localStorage.getItem('value')}
+       >
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+       </select>
+       <button onClick={()=>dispatch({type: 'byamount', payload:{amount}})}>{localStorage.getItem('value')||amount}</button>
+       {/* <button onClick={localStorage.removeItem('value')}>Clear One</button> */}
+       <button>Clear All</button>
     </div>
   )
 }
 
-export default Root
+export default Root;
